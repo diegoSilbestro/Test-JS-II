8659f784846259b5bfa8e3f54342d950c846ff0c
diegoSilbestro <diegosilbestro@gmail.com>
2022-06-27 15:03:57 -0300

    se cargan ejercicios resueltos

---
+++ JavaScript-5/tareas/tareas.js
@@ -0,0 +1,108 @@
+// No cambies los nombres de las funciones.
+
+function crearUsuario() {
+  // Crea una Clase de ES6 o una función constructor llamada "Usuario"
+  // Debe aceptar un objeto "opciones" con las propiedades "usuario", "nombre", "email" y "password"
+  // En el `contructor`, define el usuario, el nombre, el email y la contraseña
+  // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
+  // {{nombre}} debe ser el nombre definido en cada instancia
+  // Devuelve la clase
+  // Tu código:
+
+  function usuario(opciones) {
+    this.nombre = opciones.nombre;
+    this.usuario = opciones.usuario;
+    this.email = opciones.email;
+    this.password = opciones.password;
+  }
+  usuario.prototype.saludar = function () {
+    return 'Hola, mi nombre es ' + this.nombre;
+  }
+  return usuario;
+}
+
+
+function agregarMetodoPrototype(Constructor) {
+  // Agrega un método al Constructor del `prototype`
+  // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
+  // Tu código:
+  Constructor.prototype.saludar = function () { return 'Hello World!'; }
+
+
+}
+
+function agregarStringInvertida() {
+  // Agrega un método al prototype de String que devuelva la misma cadena de caracteres, pero invertida.
+  // El método debe llamarse "reverse"
+  // Ej: 'menem'.reverse() => menem
+  // 'alumno'.reverse() => 'onmula'
+  // Pista: Necesitarás usar "this" dentro de "reverse"
+
+  String.prototype.reverse = function () {
+    let str = this;
+    arrayStr = str.split('');
+    let reverseArray = [];
+    for (let i = 1; i <= arrayStr.length; i++) {
+      reverseArray.push(arrayStr[arrayStr.length - i]);
+    }
+    return reverseArray.join('');
+  }
+
+
+
+}
+// ---------------------------------------------------------------------------//
+//Crea el constructor de la clase "Persona"
+//Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio"
+//Debe tener un método llamado "detalle" que nos devuelve un objeto con las propiedades de la persona y sus valores.
+//Ej: { 
+//   Nombre: 'Juan',
+//   Apellido: 'Perez',
+//   Edad: 22,
+//   Domicilio: 'Saavedra 123'
+//  }
+
+class Persona {
+  constructor(nombre, apellido, edad, domicilio) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.domicilio = domicilio;
+  }
+  detalle() {
+    let objeto = {
+      Nombre: this.nombre,
+      Apellido: this.apellido,
+      Edad: this.edad,
+      Domicilio: this.domicilio
+    }
+    return objeto;
+  }
+}
+
+function crearInstanciaPersona(nombre, apellido, edad, dir) {
+  //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
+  //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
+  //Devolver la nueva persona creada
+  let persona1 = new Persona('Juan', 'Perez', 22, 'Saavedra 123');
+  return persona1;
+}
+
+function agregarMetodo() {
+  //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
+  //Ej: "Juan, 22 años"
+  Persona.prototype.datos = function () { return `${this.nombre}, ${this.edad} años` }
+}
+
+
+// No modificar nada debajo de esta línea, sino no correrán los test!
+// --------------------------------
+
+module.exports = {
+  crearUsuario,
+  agregarMetodoPrototype,
+  agregarStringInvertida,
+  crearInstanciaPersona,
+  agregarMetodo,
+  Persona
+};
---
+++ JavaScript-6/tareas/tareas.js
@@ -0,0 +1,71 @@
+// No cambies los nombres de las funciones
+
+function mayuscula(nombre) {
+  //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
+  //ej: Recibe "mario" ----> Devuelve "Mario"
+  //Tu código:
+  let str1 = nombre.charAt(0);
+  let str2 = nombre.slice(1);
+  return `${str1.toUpperCase() + str2}`
+}
+
+function invocarCallback(cb) {
+  // Invoca al callback `cb`
+  //Tu código:
+  cb();
+}
+
+function operacionMatematica(n1, n2, cb) {
+  //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
+  //Devolver el callback pasándole como argumentos los números recibidos.
+  //Tu código:
+  cb(n1, n2);
+}
+
+function sumarArray(numeros, cb) {
+  // Suma todos los números enteros (int/integers) de un array ("numeros")
+  // Pasa el resultado a `cb`
+  // No es necesario devolver nada
+  //Tu código:
+  let sum = 0;
+  numeros.forEach(element => { sum += element });
+  cb(sum);
+}
+
+function forEach(array, cb) {
+  // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
+  // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+  //Tu código:
+  array.forEach(element => cb(element));
+}
+
+function map(array, cb) {
+  // Crea un nuevo array
+  // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
+  // El nuevo array debe tener la misma longitud que el array del argumento
+  //Tu código:
+  let nuevoArray = [];
+  nuevoArray= array.map(x => cb(x));
+  return nuevoArray;
+}
+
+function filter(array) {
+  //Filtrar todos los elementos del array que comiencen con la letra "a".
+  //Devolver un nuevo array con los elementos que cumplen la condición
+  //Tu código:
+  return array.filter(x =>  x.charAt(0) == 'a');
+  
+}
+
+// No modificar nada debajo de esta línea, sino no correrán los test!
+// --------------------------------
+
+module.exports = {
+  mayuscula,
+  invocarCallback,
+  operacionMatematica,
+  sumarArray,
+  forEach,
+  map,
+  filter
+};
